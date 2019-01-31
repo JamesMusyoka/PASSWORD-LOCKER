aaa165fd094143e819c2a2093c3a13b5ffd5738b
James Musyoka <jamesmu475@gmail.com>
2019-01-31 10:03:51 +0300

    functionality

--- users.py
+++ users.py
@@ -1,68 +1,74 @@
 import pyperclip
-
-class User:
+class Users:
     '''
     Class that generates new instances of a user.
     '''
 
     user_list = []
 
-    def __init__(self, first_name, last_name, email_address, password):
+    def __init__(self, first_name, last_name, email, password):
         '''
         This method helps us define properties for our objects.
         '''
 
         self.first_name = first_name
         self.last_name = last_name
-        self.email_address = email_address
+        self.email = email
         self.password = password
 
 
-    def save_user(self):
+    def save_users(self):
         '''
-        This method saves user objects into the user_list.
-        '''
-
-        User.user_list.append(self)
-
-    def delete_user(self):
-        '''
-        This method deletes a saved user from the user_list.
+        This method saves Users objects into the user_list.
         '''
 
-        User.user_list.remove(self)
+        Users.user_list.append(self)
+
+    def delete_users(self):
+        '''
+        This method deletes a saved users from the user_list.
+        '''
+
+        Users.user_list.remove(self)
 
     @classmethod
-    def find_by_email_address(cls, email_address):
+    def find_by_email(cls, email):
         '''
         This method takes an email and returns user data that matches the email.
         Args:
-            email: email address to search
+            email: email  to search
         Returns:
-            user information
+            users information
         '''
 
-        for user in cls.user_list:
-            if user.email_address == email_address:
-                return user
+        for users in cls.user_list:
+            if users.email == email:
+                return users
 
     @classmethod
-    def user_exists(cls,email_address):
+    def user_exist(cls,email):
         '''
-        This method checks if user exists from list.
+        This method checks if users exists from list.
         Args:
-        email: email address to search if it exists.
+        email: email to search if it exists.
         Returns:
         True or false depending on if it exists.
         '''
 
-        for user in cls.user_list:
-            if user.email_address == email_address:
+        for users in cls.user_list:
+            if users.email == email:
                 return True
 
             return False
 
-    # @classmethod
-    # def copy_email_address(cls, email_address):
-    #     user_found = User.find_by_email_address(email_address)
-    #     pyperclip.copy(user_found.email_address)
+    @classmethod
+    def display_users(cls):
+        '''
+        method that returns the users list
+        '''
+        return cls.user_list
+
+    @classmethod
+    def copy_email(cls, email):
+        users_found = Users.find_by_email("jamesmu475@gmail.com")
+        pyperclip.copy(users_found.email)
